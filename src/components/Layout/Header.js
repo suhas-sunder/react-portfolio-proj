a65e885600791dde8358cebd6bedffba3c0c313c
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-05-10 21:11:21 -0400

    style: project section, header section
    Add image to header section and reformat overall style.
    
    Add image to projects section and reformat style.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,50 +1,55 @@
 import React from "react";
 import Styles from "./Header.module.css";
 import Button from "../UI/Button";
+import ProfilePic from "../../assets/profile_pic.jpg";
 
 function Header() {
   return (
     <div className={Styles.header}>
-      <img alt="head-shot of Suhas Sunder" src="" />
-      <div>
+      <img
+        alt="head-shot of Suhas Sunder"
+        src={ProfilePic}
+        className={Styles["profile-img"]}
+      />
+      <div className={Styles.profile}>
         <h1>
           Hi, I'm Suhas <span className={Styles.highlight}>|</span> Front-end
           Developer
         </h1>
         <h3>I have a passion for learning, creating, and problem-solving.</h3>
         <p>
-          About me. Here is some interesting information about me. Here is some interesting information about me. Here is some
+          About me. Here is some interesting information about me. Here is some
           interesting information about me. Here is some interesting information
-          about me. <span className={Styles.highlight}>View bio to learn more...</span>
+          about me. Here is some interesting information about me.{" "}
+          <span className={Styles.highlight}>View bio to learn more...</span>
         </p>
-        
+        <ul className={Styles.links}>
+          <li>
+            <Button
+              text=""
+              logo="linkedin"
+              type="logoLink"
+              url="https://www.linkedin.com/in/s-sunder/"
+              target="_blank"
+            />
+          </li>
+          <li>
+            <Button
+              text=""
+              logo="github"
+              type="logoLink"
+              url="https://github.com/suhas-sunder"
+              target="_blank"
+            />
+          </li>
+          <li>
+            <Button text="" logo="email" type="logoLink" url="#footer" />
+          </li>
+          <li>
+            <Button text="" logo="certificates" type="logoLink" url="#footer" />
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Button
-            text=""
-            logo="linkedin"
-            type="logoLink"
-            url="https://www.linkedin.com/in/s-sunder/"
-            target="_blank"
-          />
-        </li>
-        <li>
-          <Button
-            text=""
-            logo="github"
-            type="logoLink"
-            url="https://github.com/suhas-sunder"
-            target="_blank"
-          />
-        </li>
-        <li>
-          <Button text="" logo="email" type="logoLink" url="#footer" />
-        </li>
-        <li>
-          <Button text="" logo="certificates" type="logoLink" url="#footer" />
-        </li>
-      </ul>
     </div>
   );
 }
--- src/components/UI/ImageCard.js
+++ src/components/UI/ImageCard.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Styles from "./ImageCard.module.css";
 import Button from "./Button";
+import Screenshot from "../../assets/eme-website-screenshot.jpg";
 
 function ImageCard() {
   return (
-    <div className={Styles.imageCard}>
-      <img className={Styles.image} />
-      <Button
-        text="project overview"
-        type="project-link"
-        logo="arrow"
-        url={"www.google.com"}
-      />
+    <div className={Styles["img-card"]}>
+    <img src={Screenshot} alt="website" className={Styles.img} />
+      <div className={Styles["btn-background"]}>
+        <Button
+          text="project overview"
+          type="project-link"
+          logo="arrow"
+          url={"www.google.com"}
+        />
+      </div>
     </div>
   );
 }

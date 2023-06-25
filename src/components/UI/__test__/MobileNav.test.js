319f75491e3e6d8a00e1c09d3b24bf78231918a1
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-06-25 00:18:51 -0400

    Update: Tests for mobile nav
    - Add tests to check elements and links when burger menu is toggled.

--- src/components/UI/__test__/MobileNav.test.js
+++ src/components/UI/__test__/MobileNav.test.js
@@ -19,7 +19,7 @@ const btnURL = [
   "https://drive.google.com/file/d/1FMej8K-ZoM7OjsTorZ8w6sEI2d9UByxB/view?usp=drive_link",
 ];
 
-describe("Default Navigation Bar", () => {
+describe("Mobile Nav bar defaults", () => {
   it("Should render atleast one link", () => {
     render(<MockNavBar />);
     const navElement = screen.getAllByTestId(/btn-link-/i);
@@ -38,13 +38,75 @@ describe("Default Navigation Bar", () => {
     expect(navElement).not.toBeInTheDocument();
   });
 
+  it("Should only render 1 link (logo button) when burger menu is closed", () => {
+    render(<MockNavBar />);
+    const navLinks = screen.getAllByTestId(/btn-link/i);
+    expect(navLinks).toHaveLength(1);
+  });
+});
+
+describe("Mobile Nav Bar click events", () => {
+  it("Should have burger closed icon visible when menu is open and open icon should be hidden", () => {
+    render(<MockNavBar />);
+    const navElement = screen.getByTestId(/burgerBtn-open/i);
+    fireEvent.click(navElement);
+    const navElementTwo = screen.queryByTestId(/burgerBtn-close/i);
+    expect(navElement).not.toBeInTheDocument();
+    expect(navElementTwo).toBeInTheDocument();
+  });
+
+  it("Should render 6 links when burger menu is open", () => {
+    render(<MockNavBar />);
+    const navElement = screen.getByTestId(/burgerBtn-open/i);
+    fireEvent.click(navElement);
+    const navLinks = screen.getAllByTestId(/btn-link/i);
+    expect(navLinks).toHaveLength(5);
+  });
+
   it("Should render links with appropriate url once burger menu is open", () => {
     render(<MockNavBar />);
     const navElement = screen.getByTestId(/burgerBtn-open/i);
-    fireEvent.click(navElement)
+    fireEvent.click(navElement);
     const navLinks = screen.getAllByTestId(/btn-link/i);
     navLinks.map((element, index) =>
       expect(element).toHaveAttribute("href", btnURL[index])
     );
+  });
+
+  it("Should only render 1 link (logo button) when burger menu is toggled open then close, meaning menu is closed", () => {
+    render(<MockNavBar />);
+    const navElement = screen.getByTestId(/burgerBtn-open/i);
+    fireEvent.click(navElement);
+    const navElementTwo = screen.getByTestId(/burgerBtn-close/i);
+    fireEvent.click(navElementTwo);
+    const navLinks = screen.getAllByTestId(/btn-link/i);
+    expect(navLinks).toHaveLength(1);
+  });
+
+  it("Should render transparent overlay background when mobile menu is open", () => {
+    render(<MockNavBar />);
+    const navElement = screen.getByTestId(/burgerBtn-open/i);
+    fireEvent.click(navElement);
+    const navElementTwo = screen.getByTestId(/mobile-nav-bkgd/i);
+    expect(navElementTwo).toBeInTheDocument();
+  });
+
+  it("Should hide mobile nav transparent background when it is clicked", () => {
+    render(<MockNavBar />);
+    const navElement = screen.getByTestId(/burgerBtn-open/i);
+    fireEvent.click(navElement);
+    const navElementTwo = screen.getByTestId(/mobile-nav-bkgd/i);
+    fireEvent.click(navElementTwo);
+    expect(navElementTwo).not.toBeInTheDocument();
+  });
+
+  it("Should close mobile nav menu when background when it is clicked, displaying only one link meaning menu is closed", () => {
+    render(<MockNavBar />);
+    const navElement = screen.getByTestId(/burgerBtn-open/i);
+    fireEvent.click(navElement);
+    const navElementTwo = screen.getByTestId(/mobile-nav-bkgd/i);
+    fireEvent.click(navElementTwo);
+    const navLinks = screen.getAllByTestId(/btn-link/i);
+    expect(navLinks).toHaveLength(1);
   });
 });

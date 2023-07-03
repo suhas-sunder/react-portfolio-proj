8c73ffd94336e536f27a2607f42ff6c90d81b7d3
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-03 14:46:45 -0400

    fix: Mobile Nav Test
    - Remove getByTestID and replace with appropriate query function such as getByLabelText or getByRole.
    
    - Separate tests by description based on form functionality being tested.
    
    - Add tests to check if each input element (name, email, phone) renders as intended.

--- src/components/UI/__test__/FormInput.test.js
+++ src/components/UI/__test__/FormInput.test.js
@@ -2,58 +2,93 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import FormInput from "../FormInput";
 
-describe("Button", () => {
-  // Test text area
+describe("Renders form elements", () => {
+  it("should display an input field by default regardless of name prop", () => {
+    render(<FormInput />);
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).toBeInTheDocument();
+  });
+
   it("should display textarea if included in props", () => {
-    render(<FormInput name="message" />);
-    const inputTextElement = screen.getByTestId(/textarea/i);
+    render(<FormInput name="message" label="message" id="message" />);
+    const inputTextElement = screen.getByLabelText(/message/i);
     expect(inputTextElement).toBeInTheDocument();
   });
 
+  it("should display name input field if text area is displayed", () => {
+    render(<FormInput name="name" label="name" id="name" />);
+    const inputElement = screen.getByLabelText(/name/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should display email input field if text area is displayed", () => {
+    render(<FormInput name="email" label="email" id="email" />);
+    const inputElement = screen.getByLabelText(/email/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should display phone input field if text area is displayed", () => {
+    render(<FormInput name="phone" label="phone" id="phone" />);
+    const inputElement = screen.getByLabelText(/phone/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+});
+
+describe("Form fields that should not render if props don't exist", () => {
   it("should not display textarea if not included in props", () => {
     render(<FormInput />);
-    const inputTextElement = screen.queryByTestId(/textarea/i);
+    const inputTextElement = screen.queryByLabelText(/message/i);
     expect(inputTextElement).not.toBeInTheDocument();
   });
 
+  it("should not display name input field if text area is displayed", () => {
+    render(<FormInput name="message" label="message" id="message" />);
+    const inputElement = screen.queryByLabelText(/name/i);
+    expect(inputElement).not.toBeInTheDocument();
+  });
+
+  it("should not display email input field if text area is displayed", () => {
+    render(<FormInput name="message" label="message" id="message" />);
+    const inputElement = screen.queryByLabelText(/email/i);
+    expect(inputElement).not.toBeInTheDocument();
+  });
+
+  it("should not display phone input field if text area is displayed", () => {
+    render(<FormInput name="message" label="message" id="message" />);
+    const inputElement = screen.queryByLabelText(/phone/i);
+    expect(inputElement).not.toBeInTheDocument();
+  });
+});
+
+describe("Form input fields touched state", () => {
   it("should have invalid class for textarea when touched", async () => {
-    render(<FormInput touched={true} name="message" />);
-    const inputTextElement = screen.getByTestId(/textarea/i);
+    render(
+      <FormInput touched={true} name="message" label="message" id="message" />
+    );
+    const inputTextElement = screen.getByLabelText(/message/i);
     expect(inputTextElement.classList.contains("message")).toBe(true);
   });
 
   it("should have valid class for textarea when touched", async () => {
-    render(<FormInput name="message" />);
-    const inputTextElement = screen.getByTestId(/textarea/i);
+    render(<FormInput name="message" label="message" id="message" />);
+    const inputTextElement = screen.getByLabelText(/message/i);
     expect(inputTextElement.classList.contains("message")).toBe(true);
   });
 
-  it("should display input field by default regardless of name prop", () => {
-    render(<FormInput />);
-    const inputElement = screen.getByTestId(/input/i);
-    expect(inputElement).toBeInTheDocument();
-  });
-
-  it("should not display input if text area is displayed", () => {
-    render(<FormInput name="message" />);
-    const inputElement = screen.queryByTestId(/input/i);
-    expect(inputElement).not.toBeInTheDocument();
-  });
-
-  // Test input field
   it("should have invalid class for input when touched", async () => {
-    render(<FormInput touched={true} />);
-    const inputElement = screen.getByTestId(/input/i);
+    render(<FormInput touched={true} name="email" label="email" id="email" />);
+    const inputElement = screen.getByLabelText(/email/i);
     expect(inputElement.classList.contains("invalid-input")).toBe(true);
   });
 
   it("should have valid class for input when touched", async () => {
-    render(<FormInput />);
-    const inputElement = screen.getByTestId(/input/i);
+    render(<FormInput name="email" label="email" id="email" />);
+    const inputElement = screen.getByLabelText(/email/i);
     expect(inputElement.classList.contains("input")).toBe(true);
   });
+});
 
-  // Test error message
+describe("Form error message", () => {
   it("should display error if input state is touched", () => {
     render(<FormInput touched={true} />);
     const errorElement = screen.getByTestId("error");

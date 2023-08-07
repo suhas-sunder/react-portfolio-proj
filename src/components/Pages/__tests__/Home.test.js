e73ffd7b717429a0dfdcdb67a133121508517e3a
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-08-06 23:57:44 -0400

    update: home component test
    -Add additional tests to Home component to check if elements and links render as intended.

--- src/components/Pages/__tests__/Home.test.js
+++ src/components/Pages/__tests__/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { getAllByTestId, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "../Home";
 import { BrowserRouter } from "react-router-dom";
@@ -15,7 +15,7 @@ const testHref = (elements, link) => {
   elements.forEach((element) => expect(element).toHaveAttribute("href", link));
 };
 
-describe("renders header elements", () => {
+describe("renders all page elements", () => {
   it("should render a header title text", () => {
     render(<MockHome showModal={false} />);
     const headerElement = screen.getByText(/Suhas Sunder Software Developer/i);
@@ -27,9 +27,33 @@ describe("renders header elements", () => {
     const imageElement = screen.getByAltText(/head-shot of Suhas Sunder/i);
     expect(imageElement).toBeInTheDocument();
   });
+
+  it("should render list of 8 skills", () => {
+    render(<MockHome showModal={false} />);
+    const skillElements = screen.getAllByTestId(/skill-icon-/i);
+    expect(skillElements).toHaveLength(8);
+  });
+
+  it("should render a summary of 4 projects", () => {
+    render(<MockHome showModal={false} />);
+    const projElements = screen.getAllByTestId(/proj-section/i);
+    expect(projElements).toHaveLength(4);
+  });
+
+  it("should render a contact form with 4 input elements", () => {
+    render(<MockHome showModal={false} />);
+    const formElement = screen.getByRole("form");
+    expect(formElement).toBeInTheDocument();
+  });
+
+  it("should render a footer element", () => {
+    render(<MockHome showModal={false} />);
+    const footerElement = screen.getByText(/2023 | Designed & coded by/i);
+    expect(footerElement).toBeInTheDocument();
+  });
 });
 
-describe("renders links", () => {
+describe("renders links/buttons with proper redirect/action", () => {
   it("should render two linkedin buttons in the document with proper links", () => {
     render(<MockHome showModal={false} />);
     const modalBtnElements = screen.getAllByTestId(/btn-link-linkedin/i);
@@ -69,11 +93,33 @@ describe("renders links", () => {
 
   it("should render a modal when modal state is true", () => {
     render(<MockHome showModal={true} />);
-    let modal = screen.getByText(/Graduated: Apr 2019/i);
-    expect(modal).toBeInTheDocument();
-  });  
-});
+    let modalElement = screen.getByText(/Graduated: Apr 2019/i);
+    expect(modalElement).toBeInTheDocument();
+  });
 
-// describe("render skills list", () => {
-//   it("should render ")
-// });
+  it("should include 4 project overview buttons", () => {
+    render(<MockHome showModal={false} />);
+    let projectElements = screen.getAllByText(/project overview/i);
+    expect(projectElements).toHaveLength(4);
+  });
+
+  it("should render a submit button for contact form", () => {
+    render(<MockHome showModal={false} />);
+    const formElement = screen.getByRole("button", { name: /submit/i });
+    expect(formElement).toBeInTheDocument();
+  });
+
+  it("should render an appropriate email link on contact form", () => {
+    render(<MockHome showModal={false} />);
+    const formElement = screen.getByRole("link", { name: /suhas@live.ca/i });
+    expect(formElement).toBeInTheDocument();
+    expect(formElement).toHaveAttribute("href", "mailto:suhas@live.ca");
+  });
+
+  it("should render an appropriate footer home link", () => {
+    render(<MockHome showModal={false} />);
+    const footerElement = screen.getByRole("link", { name: /suhas sunder/i });
+    expect(footerElement).toBeInTheDocument();
+    expect(footerElement).toHaveAttribute("href", "/");
+  });
+});

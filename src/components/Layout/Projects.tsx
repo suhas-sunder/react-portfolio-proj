585e6b30eb40fdceb49d29ff53c78f4041dc36cb
suhas-sunder <suhas@live.ca>
2024-01-19 02:53:06 -0500

    style: projects
    -Add styling to elements in project section to make sure all elements are responsive. -Fix styling that breaks the page on smaller screens.

--- src/components/Layout/Projects.tsx
+++ src/components/Layout/Projects.tsx
@@ -2,32 +2,28 @@ import Styles from "./styles/Projects.module.css";
 import ImageCard from "../UI/ImageCard";
 import TechStack from "./TechStack";
 import ProjData from "../../data/ProjData";
-import ProjectDates from "./ProjectDates";
 
 function Projects() {
   return (
-    <div id="projects" className={`${Styles.container}`}>
-      <h2 className={` text-6xl mt-16 mb-28 uppercase`}>Projects</h2>
+    <div id="projects" className="flex flex-col max-w-[1200px] mx-auto px-6 gap-60 mb-52">
+      <h2 className={` text-6xl mt-16 uppercase`}>Projects</h2>
       {ProjData.map((data, index) => (
         <div
           data-testid={`proj-section_${data.projId}`}
           id={data.projId}
           key={data.id}
-          className={`${Styles[data.projCSS]}`}
+          className={`${
+            index % 2 === 0
+              ? "lg:flex-row-reverse flex-col-reverse"
+              : "lg:flex-row flex-col-reverse"
+          } ${index === 0 && "-mt-6"} flex gap-16 mx-8 lg:mx-0`}
         >
           <div className={Styles.details}>
             <p className={Styles["project-description"]}>{data.description}</p>
             <TechStack skillsList={data.techStack} />
           </div>
-          <div className="flex relative">
-            <ProjectDates year={data.year} endYear={data.endYear} index={index} />
-            <ImageCard
-              imageURL={data.imageURL}
-              projLink={data.projLink}
-              projName={data.title}
-              projURL={data.projURL}
-            />
-          </div>
+
+          <ImageCard data={data} index={index} />
         </div>
       ))}
     </div>

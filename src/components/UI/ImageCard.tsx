00ebe4f6f84db085e53ec0412b9bc25ca3f55e0d
suhas-sunder <suhas@live.ca>
2024-01-19 02:53:06 -0500

    style: projects
    -Add styling to elements in project section to make sure all elements are responsive. -Fix styling that breaks the page on smaller screens.

--- src/components/UI/ImageCard.tsx
+++ src/components/UI/ImageCard.tsx
@@ -2,21 +2,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightFromSquare as arrowUpIcon } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight as arrowIcon } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import ProjectDates from "../Layout/ProjectDates";
 
-function ImageCard({ imageURL, projLink, projName, projURL }) {
+function ImageCard({ data, index }) {
   return (
-    <div className="flex flex-col justify-evenly items-center bg-dark-blueish-gray text-white rounded-lg min-h-[24em]">
-      <h3 className="text-2xl leading-4">{projName}</h3>
+    <div className="flex flex-col relative justify-evenly items-center bg-dark-blueish-gray text-white rounded-lg min-h-[24em] mx-auto">
+      <ProjectDates year={data.year} endYear={data.endYear} index={index} />
+      <h3 className={`${index % 2 === 0 ? "justify-end pr-14" : "justify-start pl-14"} flex w-full text-2xl leading-[1rem]`}>{data.title}</h3>
       <img
         width={350}
         height={196}
-        src={imageURL}
-        alt={`${projName} project screenshot`}
-        className="flex max-w-[350px] mx-7 rounded-lg"
+        src={data.imageURL}
+        alt={`${data.projName} project screenshot`}
+        className="flex lg:max-w-[350px]  mx-7 rounded-lg"
       />
       <div className="flex justify-evenly w-full items-center ">
         <Link
-          to={projURL}
+          to={data.projURL}
           className="flex justify-center items-center gap-3 px-4 py-3 bg-highlight-yellow text-dark-blueish-gray rounded-md font-semibold uppercase tracking-wider text-sm border-2 border-highlight-yellow hover:bg-dark-blueish-gray hover:text-highlight-yellow"
         >
           <span>View Live</span>
@@ -25,7 +27,7 @@ function ImageCard({ imageURL, projLink, projName, projURL }) {
           </span>
         </Link>
         <Link
-          to={projLink}
+          to={data.projLink}
           className="flex justify-center items-center gap-3 px-4 py-3 bg-highlight-yellow text-dark-blueish-gray rounded-md  font-semibold uppercase tracking-wider text-sm border-2 border-highlight-yellow hover:bg-dark-blueish-gray hover:text-highlight-yellow"
         >
           <span>View Details</span>
